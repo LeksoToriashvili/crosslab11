1b021578c2fb7341ba52e49aa131e8e121e176fc
marbifrost <maribifrost@gmail.com>
2024-12-04 11:44:15 +0400

    added new views

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuestionViewSet, AnswerViewSet, LikeViewSet
+from .views import QuestionViewSet, AnswerViewSet, LikeViewSet, QuestionListingViewSet, QuestionByUsernameViewSet
 
 router = DefaultRouter()
 router.register('questions', QuestionViewSet, basename='questions')
 router.register(r'answers', AnswerViewSet, basename='answers')
 router.register(r'likes', LikeViewSet, basename='likes')
+router.register(r'all-questions', QuestionListingViewSet, basename='all-questions')
+router.register(r'questions-by-user', QuestionByUsernameViewSet, basename='questions-by-user')
 
 
 urlpatterns = [
